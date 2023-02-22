b8c50ed889c55e6c36ee90788a3c1647690c4ea6
cpxqgg <chen.148@wright.edu>
2023-02-21 19:37:34 -0700

    Update Vector.v

--- Vector.v
+++ Vector.v
@@ -28,3 +28,40 @@ module top_module(
     assign out[31:0] = {in[7:0], in[15:8], in[23:16], in[31:24]};
 
 endmodule
+
+//-------4: Bitwise Operators------------------------------//
+module top_module( 
+    input [2:0] a,
+    input [2:0] b,
+    output [2:0] out_or_bitwise,
+    output out_or_logical,
+    output [5:0] out_not
+);
+    assign out_or_bitwise = a | b;
+    assign out_or_logical = | (a | b);
+    assign out_not[5:3] = ~b;
+    assign out_not[2:0] = ~a;
+
+endmodule
+
+//-----------5: four input gates------------------//
+module top_module( 
+    input [3:0] in,
+    output out_and,
+    output out_or,
+    output out_xor
+);
+    assign out_and = &in;
+    assign out_or = |in;
+    assign out_xor = ^in;
+
+endmodule
+
+//--------------6: Vector concatenation operator-----------//
+module top_module (
+    input [4:0] a, b, c, d, e, f,
+    output [7:0] w, x, y, z );//
+
+    assign {w, x, y, z} = {a, b, c, d, e, f, 2'b11};
+
+endmodule
